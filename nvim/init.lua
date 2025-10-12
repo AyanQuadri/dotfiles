60a6b40fb497663006bd7ddbec9e924b9a5445e2
AyanQuadri <zenithayan@hotmail.com>
2025-10-13 03:05:54 +0530

    add neovim init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+-- ~/.config/nvim/init.lua
+-- Bootstrap & require config modules — nothing else!
+
+-- Load lazy
+require("config.lazy")
+
+-- Load options
+require("config.options")
+
+-- Load keymaps
+require("config.keymaps")
+
